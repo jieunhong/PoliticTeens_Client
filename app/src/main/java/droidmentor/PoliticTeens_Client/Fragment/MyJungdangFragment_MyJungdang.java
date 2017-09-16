f0bb3d275ce43cc93f7e08f0d8a65cd0ec1ef3ff
jieun <hyy0786@naver.com>
2017-09-16 18:47:48 +0900

    #3 25% complete

---
+++ app/src/main/java/droidmentor/PoliticTeens_Client/Fragment/MyJungdangFragment_MyJungdang.java
@@ -0,0 +1,32 @@
+package droidmentor.PoliticTeens_Client.Fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import droidmentor.PoliticTeens_Client.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MyJungdangFragment_MyJungdang extends Fragment {
+
+
+    public MyJungdangFragment_MyJungdang() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_myjungdangs_myjungdang, container, false);
+        return rootView;
+    }
+
+}
--- app/src/main/java/droidmentor/PoliticTeens_Client/Fragment/MyJungdangFragment_SearchJungdang.java
+++ app/src/main/java/droidmentor/PoliticTeens_Client/Fragment/MyJungdangFragment_SearchJungdang.java
@@ -2,42 +2,34 @@ package droidmentor.PoliticTeens_Client.Fragment;
 
 
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTabHost;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.ViewPager;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import co.ceryle.segmentedbutton.SegmentedButtonGroup;
 import droidmentor.PoliticTeens_Client.R;
-import droidmentor.PoliticTeens_Client.ViewPagerAdapter;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MyJungdangFragment extends Fragment {
+public class MyJungdangFragment_SearchJungdang extends Fragment {
 
     FragmentManager manager;  //Fragment를 관리하는 클래스의 참조변수
     FragmentTransaction tran;  //실제로 Fragment를 추가/삭제/재배치 하는 클래스의 참조변수
 
-    public MyJungdangFragment() {
+    public MyJungdangFragment_SearchJungdang() {
         // Required empty public constructor
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-
-
-        View view = inflater.inflate(R.layout.fragment_myjundangs, container, false);
+        View view = inflater.inflate(R.layout.fragment_myjungdangs_searchjungdang, container, false);
         manager = (FragmentManager) getFragmentManager();
 
         //SegmentedButtonGroup myjundang_buttonGroup = (SegmentedButtonGroup) rootView.findViewById(R.id.myjundang_buttonGroup);
@@ -47,21 +39,19 @@ public class MyJungdangFragment extends Fragment {
 
             }
         });*/
-        TextView search=(TextView)view.findViewById(R.id.search_jungdang);
-        search.setOnClickListener(new View.OnClickListener() {
+        TextView my=(TextView)view.findViewById(R.id.my_jungdang);
+        my.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 tran = manager.beginTransaction();
-                Fragment frag = new MyJungdangFragment_SearchJungdang();
-                tran.replace(R.id.my_jungdang_layout, frag);
+                Fragment frag = new MyJungdangFragment();
+                tran.replace(R.id.search_jungdang_layout, frag);
                 tran.addToBackStack(null);
-                Log.d("ㅇㄴ","dfsf");
                 tran.commit();
             }
         });
 
         return view;
-
     }
 
 }
